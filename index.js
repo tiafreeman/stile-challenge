4a2912dbaad9d75f60d71999e351022e7abcfe3b
tiafreeman <tiajane.freeman@gmail.com>
2022-04-18 14:40:32 +1000

    Create a canvas and sprite class

---
+++ index.js
@@ -0,0 +1,12 @@
+class Sprite {
+    constructor({
+        position,
+    }) {
+        this.position = position
+    }
+
+    draw() {
+        c.fillStyle = 'red';
+        c.fillRect(this.position.x, this.position.y, 50, 150)
+    }
+}
